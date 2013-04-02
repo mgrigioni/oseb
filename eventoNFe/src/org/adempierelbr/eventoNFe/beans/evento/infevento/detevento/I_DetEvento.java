a5b33d5729c8bc10b9197c61823db356e7e9d857
mgrigioni <mgrigioni@localhost>
2013-04-02 10:59:01 -0300

    Melhorias no processamento de eventos

---
+++ eventoNFe/src/org/adempierelbr/eventoNFe/beans/evento/infevento/detevento/I_DetEvento.java
@@ -0,0 +1,30 @@
+/******************************************************************************
+ * Product: OSeB http://code.google.com/p/oseb                                *
+ * Copyright (C) 2013 Mario Grigioni                                          *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ *****************************************************************************/
+package org.adempierelbr.eventoNFe.beans.evento.infevento.detevento;
+
+/**
+ * 	Detalhes de Evento
+ * 
+ * 	@author Mario Grigioni
+ *	@version $Id: DetEvento.java, v1.0 02/04/2013 10:51, mgrigioni Exp $
+ */
+public interface I_DetEvento {
+
+	public String getVersao(); // getVersao
+	
+	public String setVersao(); // setVersao
+
+	public String getDescEvento();	// getDescEvento
+
+}	//	DetEvento
